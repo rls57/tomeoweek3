e070c9dd1eaa55ba838116156644c3ce5c10783a
rls57@students.ptcollege.edu <>
2019-05-02 18:18:30 -0400

    hello

---
+++ src/helloworld1/Helloworld1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld1;
+
+/**
+ *
+ * @author rls57
+ */
+public class Helloworld1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Schlager();
+    }
+    
+    public static void Schlager(){
+        System.out.println("Robert Schlager!!");
+    }
+}
